f596e3a6d8bae4f3df1563f8311761683d0ed367
SpokoSpartan <wojtekspoton@gmail.com>
2021-11-19 21:23:50 +0100

    Add three simple starters

---
+++ payment/payment-service/src/test/java/com/spot/on/cinema/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spot.on.cinema.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
